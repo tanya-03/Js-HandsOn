af8356afea36aaada8d3ffc3b9970574c31025ba
Tanya Batra <102572066+tanya-03@users.noreply.github.com>
2024-11-26 13:27:54 +0000

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,8 @@
+let x = 6;
+let y = 3;
+console.log(x < 10 && y > 1)
+console.log(2 < "12");
+console.log("2"==2) // check only value
+console.log("2"===2)  // check value plus datatype both
+console.log("2">1) // automatic convert string to a number
+console.log(2>"1")
