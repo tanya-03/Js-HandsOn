842740a86d2fd6347619ada8641dfae68d59e631
Tanya Batra <102572066+tanya-03@users.noreply.github.com>
2024-11-26 06:42:20 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(3+3) -> we are using nodejs not browser so it will not work here
+//tc39 and mdn are best to use syntax
+/*
+
+JavaScript has 8 Datatypes
+String
+Number
+Bigint
+Boolean
+Undefined
+Null -> standalone value
+Symbol -> unique
+Object
+
+*/
+console.log(typeof undefined)  //undefined
+console.log(typeof null) //object
