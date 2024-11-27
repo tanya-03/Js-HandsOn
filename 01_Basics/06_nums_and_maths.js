d511a8b541bdd1a8fafe194815f48978d60bb920
Tanya Batra <102572066+tanya-03@users.noreply.github.com>
2024-11-27 05:37:50 +0000

    Number and Math

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,28 @@
+const score=400
+// console.log(score);
+
+const balance =new Number(100)  // print this line in chrome console
+// console.log(balance)
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+console.log(Number);
+
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
+// console.log(Number.MAX_SAFE_INTEGER);
+
+//************* MATHS **************/
+
+console.log(Math);
+console.log(Math.abs(-44));
+console.log(Math.round(89.998));
+
+console.log(Math.ceil(4.2)); //4 se thodi bhi upper value
+console.log(Math.floor(4.98)); //After round lower value
+
+console.log(Math.sqrt(121));
+
+console.log(Math.min(2,6,6.9,4));
+console.log(Math.max(2,6,6.9,4));
+
+console.log(Math.random()); // output should be in between 0 and 1
