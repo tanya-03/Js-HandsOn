150eaeb8d37617e3b5a9dcdc3ea79928d06df32e
Tanya Batra <102572066+tanya-03@users.noreply.github.com>
2024-12-02 04:33:45 +0000

    project

--- 07_projects_changeColor/script.js
+++ 07_projects_changeColor/script.js
@@ -2,7 +2,7 @@ console.log("tanya")
 const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
 
-buttons.forEach(function (button) {
+buttons.forEach((button) => {
   console.log(button);
   button.addEventListener('click', function (e) {
     console.log(e);
